1df89fb74cf39ddff7e18688de920d674a831bab
Juan Pedro Tovar <jptovar92@gmail.com>
2021-10-07 23:22:40 -0300

    Se agregan comentarios y corrige ortografía

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,35 @@
 
-alert('¿Estas listo para el desaío?')
 
+// Bienvenida.
+alert('¿Estas listo para el desafío?')
+
+// Declaración de variables.
 let jugadaUsuario
 let vecesJugar
 
+// Bucle DoWhile para pedir la cantidad de partidas, solo puede ser un número.
 do{
   vecesJugar = parseInt(prompt("Ingresa tus vidas:",'Recuerda que no puedes ser inmortal'));
 } while (isNaN(vecesJugar));
 
+
+// Desarrollo enfrentamientos.
 for(i=1; i<=vecesJugar;i++){
 
+  // Variables de texto porque, practica.
   const ganaste = 'Fuiste el ganador!'
   const perdiste = 'Fuiste el perdor TT-TT'
   const empataste = 'Han empatado...'
 
+  // Bucle DoWhile para elegir tu jugada, solo puede elegir piedra, papel o tijera.
     do {
       jugadaUsuario = prompt('Elige tu jugada, Piedra Papel o Tijera').toLowerCase();
     } while (jugadaUsuario !== 'piedra' && jugadaUsuario !== 'papel' && jugadaUsuario !== 'tijera'); 
       
-        
+        // Math.random para elegir la jugada de la máquina.
         let jugadaMaquina= Math.floor(Math.random()*3);
         
+        // Transforma el número a String.
         if(jugadaMaquina == 0){
           jugadaMaquina = 'piedra'
         }else if(jugadaMaquina == 1){
@@ -29,6 +38,7 @@ for(i=1; i<=vecesJugar;i++){
           jugadaMaquina = 'tijera'
         }
         
+        // Compara resultado y declara ganador, vencedor o empate.
         if (jugadaUsuario === jugadaMaquina) {  
           alert(ganaste)
           console.log(`En la ronda N°${i} elegiste ${jugadaUsuario} y la máquina eligió ${jugadaMaquina}, ${ganaste.toLowerCase()}`);
